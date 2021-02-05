22136cd675e9ff58a51c8bd5f1227afedfa9f6b5
jamievullo <jvullo@protonmail.com>
2021-02-04 21:42:22 -0500

    Pilot commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
+import React from 'react'
+import { VictoryBar, VictoryChart, VictoryTheme, VictoryStack } from 'victory';
+
 import './App.css';
+import Chart from './Chart';
 
 function App() {
+
+  // colorScale=[
+  //   "#913bfa",
+  //   "#9f54fb",
+  //   "#ad6dfb",
+  //   "#bb86fc",
+  //   "#c99ffd",
+  //   "#d7b8fd",
+  //   "#e5d1fe"
+  // ]
+
+  // const data2017 = [
+  //   {quarter: 1, earnings: 13000},
+  //   {quarter: 2, earnings: 16500},
+  //   {quarter: 3, earnings: 14250},
+  //   {quarter: 4, earnings: 19000}
+  // ];
+  
+  // const data2018 = [
+  //   {quarter: 1, earnings: 15000},
+  //   {quarter: 2, earnings: 12500},
+  //   {quarter: 3, earnings: 19500},
+  //   {quarter: 4, earnings: 13000}
+  // ];
+  
+  // const data2019 = [
+  //   {quarter: 1, earnings: 11500},
+  //   {quarter: 2, earnings: 13250},
+  //   {quarter: 3, earnings: 20000},
+  //   {quarter: 4, earnings: 15500}
+  // ];
+  
+  // const data2020 = [
+  //   {quarter: 1, earnings: 18000},
+  //   {quarter: 2, earnings: 13250},
+  //   {quarter: 3, earnings: 15000},
+  //   {quarter: 4, earnings: 12000}
+  // ];
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Chart />
+      {/* <VictoryChart
+        domainPadding={20}
+        theme={VictoryTheme.material}
+      >
+        <VictoryAxis
+          tickValues={[1, 2, 3, 4]}
+          tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
+        />
+        <VictoryAxis
+          dependentAxis
+          tickFormat={(x) => (`$${x / 1000}k`)}
+        />
+        <VictoryStack
+          colorScale={"warm"}
         >
-          Learn React
-        </a>
-      </header>
+          <VictoryBar
+            data={data2017}
+            x="quarter"
+            y="earnings"
+          />
+          <VictoryBar
+            data={data2018}
+            x="quarter"
+            y="earnings"
+          />
+          <VictoryBar
+            data={data2019}
+            x="quarter"
+            y="earnings"
+          />
+          <VictoryBar
+            data={data2020}
+            x="quarter"
+            y="earnings"
+          />
+        </VictoryStack>
+      </VictoryChart> */}
     </div>
   );
 }
---
+++ src/Chart.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import { VictoryArea, VictoryChart, VictoryTheme, VictoryStack } from 'victory';
+import _ from 'lodash';
+import chunk from 'lodash/chunk';
+
+class Chart extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { data: this.getData() };
+    }
+    
+    componentDidMount() {
+        this.setStateInterval = window.setInterval(() => {
+                this.setState({ data: this.getData() });
+            }, 4000);
+        }
+    
+    getData() {
+        return _.range(7).map(() => {
+            return [
+            { x: 1, y: _.random(1, 5) },
+            { x: 2, y: _.random(1, 10) },
+            { x: 3, y: _.random(2, 10) },
+            { x: 4, y: _.random(2, 10) },
+            { x: 5, y: _.random(2, 15) }
+            ];
+        });
+    }
+    
+    render() {
+        return (
+            <VictoryChart
+                theme={VictoryTheme.material}
+                animate={{ duration: 1000 }}
+            >
+                <VictoryStack
+                    colorScale={[
+                    "#913bfa",
+                    "#9f54fb",
+                    "#ad6dfb",
+                    "#bb86fc",
+                    "#c99ffd",
+                    "#d7b8fd",
+                    "#e5d1fe"
+                    ]}
+                >
+                    {this.state.data.map((data, i) => {
+                        return (
+                            <VictoryArea
+                                key={i}
+                                data={data}
+                                interpolation={"basis"}
+                            />
+                        );
+                    })}
+                </VictoryStack>
+            </VictoryChart>
+        );
+    }
+}
+
+export default Chart
