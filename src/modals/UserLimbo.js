3037826e28a016646089dd0c5a7088b639fe9794
Jamie vullo <jvullo@protonmail.com>
2021-02-14 17:51:33 -0500

    Added image upload for UserLimbo

--- src/modals/UserLimbo.js
+++ src/modals/UserLimbo.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { Form, Button, Container, Collapse, Row, Image } from 'react-bootstrap'
+import example1 from '../images/IDSampleImage.png';
+import example2 from '../images/PassportSampleImage.png';
 
 export default function UserLimbo() {
 
@@ -16,16 +18,18 @@ export default function UserLimbo() {
     }
 
     return (
-        <Container style={{ marginTop: "2em", width: "50%" }}>
+        <Container style={{ marginTop: "2em", width: "100%" }}>
+            <h5 style={{ marginBottom: "2em" }} >Identity Verification</h5>
+                <p style={{textAlign: "left"}}>Upload an image of your government issued ID or current passport. Please ensure that all information is visible and easy to read. See samples below:</p>
             <Form>
                 { file && (
                     <Container id="upload-preview">
                         <center>
-                            <Image src={file} rounded thumbnail id="image-preview"/>
+                            <Image src={file} rounded thumbnail id="image-preview" style={{maxWidth: "80%"}}/>
                         </center>
                     </Container>
                 )}
-                <Form.Group>
+                <Form.Group style={{ marginTop: "2em"}}>
                     {/* <Form.Label>Select Image</Form.Label> */}
                     <Form.Control 
                         type="file"
@@ -34,6 +38,8 @@ export default function UserLimbo() {
                         />
                 </Form.Group>
             </Form>
+            <img src={example1} style={{ width: "50%"}} />
+            <img src={example2} style={{ width: "50%"}} />
         </Container>            
     )
 }
